f9d8a4cc1bea6321ac44b60922493ce4842fd85f
Thomas Olinger <olingerthomas95@gmail.com>
2023-12-03 16:51:34 -0500

    added graph visualization features, spaced out all elements, front end is finished

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -3,18 +3,13 @@ import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import SearchBar from "./Searchbar";
-
-import { Typography } from "@mui/material";
-<<<<<<< HEAD
-
-=======
 import Selection from "./selection";
->>>>>>> 4c3b1ba0a2d0c8d0a9751e5ece00d0d9ac1415fd
+import { Typography } from "@mui/material";
 const Menu = ({ children }) => {
   return (
     <Paper
       style={{
-        height: "96.5vh",
+        height: "100vh",
         padding: "16px",
         display: "flex",
         flexDirection: "column",
